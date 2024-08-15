13f2e6e64016b9f43ea069e8fc5d6de32944546b
Robrecht Cannoodt <rcannood@gmail.com>
2024-08-15 10:02:15 +0200

    resolve issues in r package

--- packages/r/openproblems/R/resolve_path.R
+++ packages/r/openproblems/R/resolve_path.R
@@ -1,23 +1,25 @@
 #' Resolve a path
-#' 
+#'
 #' This function will resolve a path to an absolute path. If
 #' the path is relative, it will be resolved relative to the
 #' parent path. If the path is absolute, it will be resolved
 #' relative to the project path.
-#' 
+#'
 #' @param path The path to resolve
 #' @param project_path The path to the root of the Viash project
 #' @param parent_path The path to the parent directory
 #' @return The resolved path
-#' 
+#'
+#' @importFrom fs path_abs
+#'
 #' @noRd
 #' @examples
 #' project_path <- "/path/to/project"
 #' parent_path <- "/path/to/project/subdir"
-#' 
+#'
 #' resolve_path("./file.yaml", project_path, parent_path)
 #' # "/path/to/project/subdir/file.yaml"
-#' 
+#'
 #' resolve_path("/file.yaml", project_path, parent_path)
 #' # "/path/to/project/file.yaml"
 resolve_path <- function(path, project_path, parent_path) {
