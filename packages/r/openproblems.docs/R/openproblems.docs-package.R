52d65009713d552e8dbca7bba08647f6086f6b2c
Robrecht Cannoodt <rcannood@gmail.com>
2024-08-19 12:10:44 +0200

    remove unused import

--- packages/r/openproblems.docs/R/openproblems.docs-package.R
+++ packages/r/openproblems.docs/R/openproblems.docs-package.R
@@ -4,9 +4,9 @@
 ## usethis namespace: start
 #' @importFrom dplyr bind_rows select mutate everything filter transmute group_by summarise arrange
 #' @importFrom purrr map_df pmap_chr map2_chr map_chr map
-#' @importFrom rlang .data %||% %|%
+#' @importFrom rlang .data %|% %||%
 #' @importFrom stats na.omit
-#' @importFrom tibble as_tibble lst
+#' @importFrom tibble as_tibble
 #' @importFrom stringr str_replace_all
 #' @importFrom glue glue
 #' @importFrom openproblems.utils strip_margin
