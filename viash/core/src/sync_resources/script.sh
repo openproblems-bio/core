6a3132f0c9de4a2c7c28e0925d2bc868671adb5d
Robrecht Cannoodt <rcannood@gmail.com>
2024-08-21 21:28:30 +0200

    update sync test resources

--- viash/core/src/sync_resources/script.sh
+++ viash/core/src/sync_resources/script.sh
@@ -24,16 +24,10 @@ if [ ! -z ${par_exclude+x} ]; then
   done
 fi
 
-
-# Disable the use of the Amazon EC2 instance metadata service (IMDS).
-# see https://florian.ec/blog/github-actions-awscli-errors/
-# or https://github.com/aws/aws-cli/issues/5234#issuecomment-705831465
-export AWS_EC2_METADATA_DISABLED=true
-
-
 function sync_s3() {
   local s3_path="$1"
   local dest_path="$2"
+  AWS_EC2_METADATA_DISABLED=true \
     aws s3 sync \
     "$s3_path" \
     "$dest_path" \
@@ -41,22 +35,17 @@ function sync_s3() {
     "${extra_params[@]}"
 }
 
-resources_detected=$(yq e '.info | has("test_resources")' "$par_input")
-if [ "$resources_detected" == "false" ]; then
-  echo "No test resources detected."
-  exit 0
-fi
+yq e \
+  '.info.test_resources[] | "{type: " + (.type // "s3") + ", path: " + .path + ", dest: " + .dest + "}"' \
+  "${par_input}" | \
+  while read -r line; do
+    type=$(echo "$line" | yq e '.type')
+    path=$(echo "$line" | yq e '.path')
+    dest=$(echo "$line" | yq e '.dest')
 
-# reformat the test resources into a pseudo-json that can be read line-by-line by bash
-nr_res=$( yq e '.info.test_resources | length' "$par_input")
+    echo "Syncing '$path' to '$dest'..."
 
-for ((i=0; i<nr_res; i++)); do
-  type=$(yq e ".info.test_resources[$i].type" "$par_input")
-  if [ "$type" == "s3" ]; then
-    s3_path=$(yq e ".info.test_resources[$i].path" "$par_input")
-    dest_path=$(yq e ".info.test_resources[$i].dest" "$par_input")
-    sync_s3 "$s3_path" "$dest_path"
-  fi
-done
-
-cp -r resources_test/ "$par_output"
+    if [ "$type" == "s3" ]; then
+      sync_s3 "$path" "$par_output/$dest"
+    fi
+  done
