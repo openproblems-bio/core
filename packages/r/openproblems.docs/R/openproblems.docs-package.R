5e2d27178b2590ef028dac199b16bd49035972c8
Robrecht Cannoodt <rcannood@gmail.com>
2024-09-23 09:23:58 +0200

    fix rendering functions (#19)
    * fix rendering functions
    
    * fix styling issues
    
    * fix styling issues

--- packages/r/openproblems.docs/R/openproblems.docs-package.R
+++ packages/r/openproblems.docs/R/openproblems.docs-package.R
@@ -2,11 +2,11 @@
 "_PACKAGE"
 
 ## usethis namespace: start
-#' @importFrom dplyr bind_rows select mutate everything filter transmute group_by summarise arrange
+#' @importFrom dplyr bind_rows select mutate everything filter transmute group_by summarise arrange case_when
 #' @importFrom purrr map_dfr map2_chr map_chr map
 #' @importFrom rlang .data %|% %||%
 #' @importFrom stats na.omit
-#' @importFrom tibble as_tibble
+#' @importFrom tibble as_tibble rownames_to_column
 #' @importFrom stringr str_replace_all
 #' @importFrom glue glue
 #' @importFrom openproblems.utils strip_margin validate_object
